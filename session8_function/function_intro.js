3d5104b4016baee5eab654cb2b387b5d6b4846bf
Hung Nguyen Xuan <hungnx@pixelz.com>
2019-11-29 11:06:27 +0700

    session 8 function

---
+++ session8_function/function_intro.js
@@ -0,0 +1,51 @@
+function welcome() { // Khai báo function
+  console.log("Hello world");
+}
+
+// welcome(); // Gọi sử dụng function
+
+// welcome();
+// welcome();
+
+let xinChao = function () {
+  print("Xin chào");
+}
+
+// xinChao();
+
+function tinhTong(a, b){
+  print(a);
+  print(b);
+  print(`Tong 2 so la ${a + b}`);
+}
+// tinhTong(10, 20);
+
+let soThuNhat = 10;
+let soThuHai = 20;
+tinhTong(soThuHai, soThuNhat);
+function tinhHieu(a, b, c){
+
+}
+function tinhTich(a, b){
+
+}
+function tinhThuong(a, b){
+
+}
+
+function print(message){
+  // console.log(message);
+  alert(message);
+}
+
+function tinhTong2(a, b){
+  return a + b;
+}
+
+let sum = tinhTong2(soThuNhat, soThuHai);
+print(sum);
+
+// chỉ số BMI, nhập vào chiều cao và cân nặng
+// in ra màn hình trạng thái của người dùng
+
+
